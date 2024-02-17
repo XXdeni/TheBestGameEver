b028ac68a74b29171c040cd341cb48b0f5aee70d
XXdeni <arslan.den.09@gmail.com>
2024-02-17 23:30:23 +0500

    GoblinAI

---
+++ Assets/script/EnemyAI.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    private NavMeshAgent _navMeshAgent;
+  
+    public List<Transform> patrolPoints;
+
+
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+        PickNewPatrolPoint();
+    }
+
+    
+    void Update()
+    {
+        
+        if(_navMeshAgent.remainingDistance == 1)
+        {
+            PickNewPatrolPoint();
+        }
+    }
+    private void PickNewPatrolPoint()
+    {
+        _navMeshAgent.destination = patrolPoints[Random.Range(0, patrolPoints.Count)].position;
+    }
+
+}
